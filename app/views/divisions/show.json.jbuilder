997b80bddb22948d37b8b20eb099e4d82594ec1e
Prelang Builder <builder@prelang.com>
2014-07-04 13:54:01 +0000

    Generated scaffolding for model 'Division' via Rails

---
+++ app/views/divisions/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @division, :id, :name, :garden_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :divisions
+
   resources :prune_types
 
   resources :yield_categories
